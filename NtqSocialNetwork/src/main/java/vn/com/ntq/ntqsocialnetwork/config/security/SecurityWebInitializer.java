079e41c217d03877b49be36ce11f195cf5ccc3fc
PhamHuuMai <huumaipham1996@gmail.com>
2017-07-03 15:44:24 +0700

    delete

--- NtqSocialNetwork/src/main/java/vn/com/ntq/ntqsocialnetwork/config/security/SecurityWebInitializer.java
+++
@@ -1,7 +0,0 @@
-package vn.com.ntq.ntqsocialnetwork.config.security;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebInitializer extends AbstractSecurityWebApplicationInitializer {
-
-}
--- NtqSocialNetwork/src/main/java/vn/com/ntq/ntqsocialnetwork/entities/AbstractEntity.java
+++
@@ -1,5 +0,0 @@
-package vn.com.ntq.ntqsocialnetwork.entities;
-
-public abstract class AbstractEntity {
-
-}
